1fa0a4e6a70c2805ea4fcbb9358c9fd470eb5c07
Eric Sanchez <unit57@Erics-MacBook-Pro.local>
2017-05-24 10:40:10 -0400

    created react routes

--- memedr-react/src/App.js
+++ memedr-react/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, NavLink, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink, Link, Switch } from 'react-router-dom';
 import './App.css';
 import axios from 'axios';
 
@@ -15,12 +15,25 @@ export default class App extends Component {
   render() {
     return (
       <div className="App-header">
-        <Landing/>
-        <About/>
-        <SignUp/>
-        <Profile/>
-        <Main/>
-        <Matches/>
+	     <Router>
+	     	<div id="wrapper">
+		        <NavLink to="/">Home</NavLink>&nbsp;&nbsp;
+		        <NavLink to="/about">About</NavLink>&nbsp;&nbsp;
+		        <NavLink to="/signup">Sign Up</NavLink>&nbsp;&nbsp;
+		        <NavLink to="/profile">Profile</NavLink>&nbsp;&nbsp;
+		        <NavLink to="/main">Main</NavLink>&nbsp;&nbsp;
+		        <NavLink to="/matches">Matches</NavLink>&nbsp;&nbsp;
+		       <Switch>
+		       		<Route path="/" exact component={Landing}></Route>
+		       		<Route path="/about" component={About}></Route>
+		       		<Route path="/signup" component={SignUp}></Route>
+		       		<Route path="/profile" component={Profile}></Route>
+		       		<Route path="/main" component={Main}></Route>
+		       		<Route path="/matches" component={Matches}></Route>
+		       </Switch>
+		      </div>
+	      </Router>        
+
       </div>
     );
   }
