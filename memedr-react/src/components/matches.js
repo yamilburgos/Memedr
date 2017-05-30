1b1eb05261807f834b78d63649414d57fc603a97
Student <student@GAs-MacBook-Air-3.local>
2017-05-29 23:02:28 -0400

    Fixed a match rendering oversight

--- memedr-react/src/components/matches.js
+++ memedr-react/src/components/matches.js
@@ -23,41 +23,40 @@ export default class Matches extends Component {
       id: id,
       username: username
     }).catch((err) => { return err });
-  }
+  }  
 
 // this route is doesnt render the matches itself but is the parent of match list to which we pass props to// remember this: Matches renders Matchlist which renders Match
   render() {
-    if (this.props.chosenMeme !== -1) {
-    return (
-      <div className="matchesBorder bigBorder">
-        <div className="tempBorder">
-          <div className="navButtons">
-            <NavLink to="/profile"><button className="btn btn-info" type="submit">My Profile</button></NavLink>&nbsp;&nbsp;
-            <NavLink to="/main"><button className="btn btn-info" type="submit">Pick Memes</button></NavLink>
+    if (this.props.chosenMeme !== -1 && this.props.matches.length > 0) {
+      return (
+        <div className="matchesBorder bigBorder">
+          <div className="tempBorder">
+            <div className="navButtons">
+              <NavLink to="/profile"><button className="btn btn-info" type="submit">My Profile</button></NavLink>&nbsp;&nbsp;
+              <NavLink to="/main"><button className="btn btn-info" type="submit">Pick Memes</button></NavLink>
+            </div>
+            <MatchesList matches={this.props.matches}
+              deleteMatch={this.deleteMatch}
+              disabled={this.props.disabled}
+              response={this.props.userData}
+              toggleDisabled={this.props.toggleDisabled} />
           </div>
-          <MatchesList matches={this.props.matches}
-            deleteMatch={this.deleteMatch}
-            disabled={this.props.disabled}
-            response={this.props.userData}
-            toggleDisabled={this.props.toggleDisabled} />
         </div>
-      </div>
-    );
-  }
-  else {
-        return (
-      <div className="matchesBorder bigBorder">
-        <div className="tempBorder">
-          <div className="navButtons">
-            <NavLink to="/profile"><button className="btn btn-info" type="submit">My Profile</button></NavLink>&nbsp;&nbsp;
-            <NavLink to="/main"><button className="btn btn-info" type="submit">Pick Memes</button></NavLink>
+      );
+    }
+
+    else {
+      return (
+        <div className="matchesBorder bigBorder">
+          <div className="tempBorder">
+            <div className="navButtons">
+              <NavLink to="/profile"><button className="btn btn-info" type="submit">My Profile</button></NavLink>&nbsp;&nbsp;
+              <NavLink to="/main"><button className="btn btn-info" type="submit">Pick Memes</button></NavLink>
+            </div>
+              <center><h1>Sorry, no matches yet. Like a meme!</h1></center>
           </div>
-                <div>
-                    <center><h1>Sorry, no matches yet. Like a meme!</h1></center>
-                </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
 }
